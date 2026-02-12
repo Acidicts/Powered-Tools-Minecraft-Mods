c2040f657c7b5fd6315efd6fb7949e364bc972d1
Acidicts <alexjhayes1@hotmail.com>
2026-02-12 17:07:33 +0000

    feat: Enhance Powered Sword with battery status display and color indicators

--- src/main/java/net/acidicts/poweredtools/item/custom/PoweredSword.java
+++ src/main/java/net/acidicts/poweredtools/item/custom/PoweredSword.java
@@ -8,6 +8,7 @@ import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SwordItem;
 import net.minecraft.item.ToolMaterial;
+import net.minecraft.nbt.NbtCompound;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.text.Text;
@@ -51,5 +52,43 @@ public class PoweredSword extends SwordItem implements BatteryUsing {
             return TypedActionResult.success(stack);
         }
         return super.use(world, user, hand);
+    }
+
+    @Override
+    public boolean isItemBarVisible(ItemStack stack) {
+        return true;
+    }
+
+    @Override
+    public int getItemBarStep(ItemStack stack) {
+        NbtCompound nbt = getBatteryData(stack);
+        if (nbt.getBoolean("battery_installed")) {
+            int energy = getEnergy(stack);
+            int maxCapacity = getMaxCapacity(stack);
+            if (maxCapacity == 0) return 0;
+            return Math.round(13.0F * energy / maxCapacity);
+        }
+        return (int) 13.0F;
+    }
+
+    @Override
+    public int getItemBarColor(ItemStack stack) {
+        NbtCompound nbt = getBatteryData(stack);
+        if (nbt.getBoolean("battery_installed")) {
+            int energy = getEnergy(stack);
+            int maxCapacity = getMaxCapacity(stack);
+            if (maxCapacity == 0) return 0xFF0000;
+
+            float ratio = (float) energy / maxCapacity;
+
+            if (ratio > 0.5F) {
+                return 0x00FFFF;
+            } else if (ratio > 0.25F) {
+                return 0xFFFF00;
+            } else {
+                return 0xFF0000;
+            }
+        }
+        return 0xFF0000;
     }
 }
